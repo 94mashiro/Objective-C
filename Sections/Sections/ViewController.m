3b0598f1b4cdaf153204d8b10456734cdf413db9
Mashiro <Hyperoot@gmail.com>
2015-10-03 23:07:55 +0800

    Add SearchBar

--- Sections/Sections/ViewController.m
+++ Sections/Sections/ViewController.m
@@ -17,7 +17,10 @@ static NSString *SectionsTableIdentifier = @"SectionsTableIdentifier";
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    NSMutableArray *filteredNames;
+    UISearchDisplayController *searchController;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -38,6 +41,13 @@ static NSString *SectionsTableIdentifier = @"SectionsTableIdentifier";
         barBackground.backgroundColor = [UIColor colorWithWhite:1.0 alpha:1.0];
         [self.view addSubview:barBackground];
     }
+    
+    filteredNames = [NSMutableArray array];
+    UISearchBar *searchBar = [[UISearchBar alloc] initWithFrame:CGRectMake(0, 0, 320, 44)];
+    tableView.tableHeaderView = searchBar;
+    searchController = [[UISearchDisplayController alloc] initWithSearchBar:searchBar contentsController:self];
+    searchController.delegate = self;
+    searchController.searchResultsDataSource = self;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -50,30 +60,69 @@ static NSString *SectionsTableIdentifier = @"SectionsTableIdentifier";
 #pragma mark -
 #pragma mark Table View Data Source Methods
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return [self.keys count];
+    if (tableView.tag == 1) {
+        return [self.keys count];
+    } else {
+        return 1;
+    }
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    NSString *key = self.keys[section];
-    NSArray *nameSection = self.names[key];
-    return [nameSection count];
+    if (tableView.tag == 1) {
+        NSString *key = self.keys[section];
+        NSArray *nameSection = self.names[key];
+        return [nameSection count];
+    } else {
+        return [filteredNames count];
+    }
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-    return self.keys[section];
+    if (tableView.tag == 1) {
+        return self.keys[section];
+    } else {
+        return nil;
+    }
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:SectionsTableIdentifier forIndexPath:indexPath];
-    NSString *key = self.keys[indexPath.section];
-    NSArray *nameSection = self.names[key];
-    
-    cell.textLabel.text = nameSection[indexPath.row];
+    if (tableView.tag == 1) {
+        NSString *key = self.keys[indexPath.section];
+        NSArray *nameSection = self.names[key];
+        
+        cell.textLabel.text = nameSection[indexPath.row];
+    } else {
+        cell.textLabel.text = filteredNames[indexPath.row];
+    }
     return cell;
 }
 
 - (NSArray *)sectionIndexTitlesForTableView:(UITableView *)tableView {
-    return self.keys;
+    if (tableView.tag == 1) {
+        return self.keys;
+    } else {
+        return nil;
+    }
+}
+
+- (void)searchDisplayController:(UISearchDisplayController *)controller didLoadSearchResultsTableView:(UITableView *)tableView {
+    [tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:SectionsTableIdentifier];
+}
+
+- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString {
+    [filteredNames removeAllObjects];
+    if (searchString.length > 0) {
+        NSPredicate *predicate = [NSPredicate predicateWithBlock:^BOOL(NSString *name, NSDictionary *b) {
+            NSRange range = [name rangeOfString:searchString options:NSCaseInsensitiveSearch];
+            return range.location != NSNotFound;
+        }];
+        for (NSString *key in self.keys) {
+            NSArray *matches = [self.names[key] filteredArrayUsingPredicate:predicate];
+            [filteredNames addObjectsFromArray:matches];
+        }
+    }
+    return YES;
 }
 
 @end
